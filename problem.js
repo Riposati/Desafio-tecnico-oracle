620a29f7ae7da13f8a0a7be9bfdbbcc36624b77c
guriposa <guriposa@gmail.com>
2021-12-22 22:29:11 -0300

    inclusao arquivo testes pro node.js

--- problem.js
+++ problem.js
@@ -1,22 +1,33 @@
-let strUserInput =  "ABCDEF"
-let strFront = [], strBack = []
-let aux = strUserInput.length
-let aux2 = aux
+const fs = require('fs');
 
-for(let i=0;i<strUserInput.length;i++){
+const data = fs.readFileSync('input.txt', 'UTF-8')
+//fs.readFileSync('/dev/stdin', 'UTF-8') // NO URI / LINUX funciona assim
 
-    while(aux2 > 0){
-        strFront.push(strUserInput[i])
-        strBack.push(strUserInput[i])
-        aux2--
-    }    
-    aux2 = --aux
+const lines = data.split(/\r?\n/)
+
+while(lines.length > 1){ // eof
+
+    let strUserInput =  lines.shift()
+    let strFront = [], strBack = []
+    let aux = strUserInput.length
+    let aux2 = aux
+
+    for(let i=0;i<strUserInput.length;i++){
+
+        while(aux2 > 0){
+            strFront.push(strUserInput[i])
+            strBack.push(strUserInput[i])
+            aux2--
+        }    
+        aux2 = --aux
+    }
+
+    strBack = strBack.reverse()
+    strBack.splice(0,1)
+
+    strBack = strBack.join(",")
+    strFront = strFront.join(",")
+
+    console.log(strFront.concat(",", strBack))
+
 }
-
-strBack = strBack.reverse()
-strBack.splice(0,1)
-
-strBack = strBack.join(",")
-strFront = strFront.join(",")
-
-console.log(strFront.concat(",", strBack))
